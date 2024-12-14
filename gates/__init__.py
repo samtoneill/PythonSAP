c505f91490dbfe3b2f406214eb1962d50baa4922
Sam O'Neill <38918777+samtoneill@users.noreply.github.com>
2024-12-14 16:42:11 +0000

    Update __init__.py

--- gates/__init__.py
+++ gates/__init__.py
@@ -1,1 +1,9 @@
+# gates/__init__.py
 
+from .and_gate import AND
+from .or_gate import OR
+from .not_gate import NOT
+from .nand_gate import NAND
+from .nor_gate import NOR
+from .xor_gate import XOR
+from .xnor_gate import XNOR
